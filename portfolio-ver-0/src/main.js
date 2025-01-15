25e53aac9c4a09b1b3a0658fc440f05bfb87bd52
jyao <jyao@student.42abudhabi.ae>
2025-01-15 12:27:45 +0400

    add(main.js): add earth mesh

--- portfolio-ver-0/src/main.js
+++ portfolio-ver-0/src/main.js
@@ -8,15 +8,32 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+const canvas = document.querySelector('#index-canvas');
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#index-bg'),
+	antialias: true,
+	canvas: canvas,
+	alpha: true,
 });
 
 camera.position.setZ(30);
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+// Resize dynamically depending on windows size
+
+/* function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false); */
 
 // Torus
 
@@ -37,10 +54,10 @@ scene.add(torus);
 
 // Light sources
 
-const pointLight = new THREE.PointLight(0xFFFFFF, 1, 0, 0.01);
-pointLight.position.set(10, 10, 10);
+const pointLight = new THREE.PointLight(0xFFFFFF, 1, 0, 0);
+pointLight.position.set(0, 100, 0);
 
-const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.1);
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1);
 scene.add(pointLight, ambientLight);
 
 // Add stars
@@ -62,9 +79,47 @@ for (let count = 0; count < 200; count++) {
 	addStar();
 }
 
+// Avatar
+
+const jyaoTexture = new THREE.TextureLoader().load("../assets/images/jyao-pp.jpg")
+
+const jyaoCube = new THREE.Mesh(
+	new THREE.BoxGeometry(3, 3, 3),
+	new THREE.MeshBasicMaterial({
+		map: jyaoTexture,
+	}),
+);
+
+scene.add(jyaoCube);
+
+// Earth
+
+const earth = new THREE.Mesh(
+	new THREE.SphereGeometry(5, 64, 64),
+	new THREE.MeshPhongMaterial({
+		map: new THREE.TextureLoader().load("../assets/images/earth/earth-day.png"),
+		normalMap: new THREE.TextureLoader().load("../assets/images/earth/earth-normal-map.png"),
+		specularMap: new THREE.TextureLoader().load("../assets/images/earth/earth-specular-map.png"),
+		specular: 0x0000F0,
+	}),
+);
+
+const earthCloud = new THREE.Mesh(
+	new THREE.SphereGeometry(5.1, 64, 64),
+	new THREE.MeshStandardMaterial({
+		map: new THREE.TextureLoader().load("../assets/images/earth/earth-cloud.png"),
+		alphaMap: new THREE.TextureLoader().load("../assets/images/earth/earth-cloud.png"),
+		transparent: true,
+	}),
+);
+
+scene.add(earth, earthCloud);
+
 // Background
 
- const bgTexture = new THREE.TextureLoader().load("../assets/images/")
+// const bgTexture = new THREE.TextureLoader().load("../assets/images/landscape-grass.png");
+// bgTexture.minFilter = THREE.LinearFilter;
+// scene.background = bgTexture;
 
 // Helpers
 
@@ -87,7 +142,10 @@ function rotateMesh(mesh, x, y, z) {
 function animate() {
   requestAnimationFrame(animate);
 
-  rotateMesh(torus, 0.01, 0.005, 0.01)
+  rotateMesh(jyaoCube, 0.01, 0.01, 0.01);
+  rotateMesh(torus, 0.01, 0.005, 0.01);
+  rotateMesh(earth, 0.0001, 0, 0);
+  rotateMesh(earthCloud, 0.0005, 0.0005, 0.0005);
 
   controls.update();
 
